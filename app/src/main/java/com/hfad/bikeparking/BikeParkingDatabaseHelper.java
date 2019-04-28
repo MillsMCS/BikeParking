0088014410abfdd4b1c2115a8d2a09d0dd54174f
kikifreena <14kobarbara@gmail.com>
2019-04-28 13:31:53 -0700

    Added code to insert the bike rack
    Also fixed typo with notes/checkin on the table, and added constructor to super.

--- app/src/main/java/com/hfad/bikeparking/BikeParkingDatabaseHelper.java
+++ app/src/main/java/com/hfad/bikeparking/BikeParkingDatabaseHelper.java
@@ -1,60 +1,29 @@
 package com.hfad.bikeparking;
 
+import android.content.ContentValues;
+import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class BikeParkingDatabaseHelper extends SQLiteOpenHelper {
     private static final String DB_NAME = "bikeparking";
+    private static final int DB_VERSION = 1;
 
-    /**
-     * Called when the database is created for the first time. This is where the
-     * creation of tables and the initial population of the tables should happen.
-     *
-     * @param db The database.
-     */
-    @Override
-    public void onCreate(SQLiteDatabase db) {
-        db.execSQL("CREATE TABLE USER ( " +
-                "_id INTEGER PRIMARY KEY AUTOINCREMENT, " +
-                "NAME TEXT, " +
-                "EMAIL TEXT, " +
-                "AUTH TEXT, " +
-                "STRIKES INTEGER);");
-        db.execSQL("CREATE TABLE BIKE_RACK (" +
-                "_id INTEGER PRIMARY KEY AUTOINCREMENT," +
-                "NAME TEXT," +
-                "LATITUDE REAL," +
-                "LONGITUDE REAL," +
-                "PHOTO INTEGER," +
-                "ADDED_BY INTEGER," +
-                "NOTES NUMERIC," +
-                "FOREIGN KEY (photo) REFERENCES PHOTO(_id)," +
-                "FOREIGN KEY (added_by) REFERENCES USER(_id));");
-        db.execSQL("CREATE TABLE NOTES ( " +
-                "_id INTEGER PRIMARY KEY AUTOINCREMENT, " +
-                "RACK_ID INTEGER, " +
-                "USER_ID INTEGER, " +
-                "TIME NUMERIC, " +
-                "NOTE TEXT, " +
-                "FOREIGN KEY (rack_id) REFERENCES BIKE_RACK(_id), " +
-                "FOREIGN KEY (user_id) REFERENCES USER(_id));");
-        db.execSQL("CREATE TABLE NOTES (" +
-                "_id INTEGER PRIMARY KEY AUTOINCREMENT," +
-                "RACK_ID INTEGER," +
-                "USER_ID INTEGER," +
-                "TIME NUMERIC," +
-                "SPACE_REMAINING INTEGER," +
-                "FOREIGN KEY (rack_id) REFERENCES BIKE_RACK(_id)," +
-                "FOREIGN KEY (user_id) REFERENCES USER(_id));");
-        db.execSQL("CREATE TABLE PHOTO (" +
-                "_id INTEGER PRIMARY KEY AUTOINCREMENT," +
-                "RACK_ID INTEGER,\n" +
-                "USER_ID INTEGER,\n" +
-                "TIME NUMERIC,\n" +
-                "PHOTO BLOB,\n" +
-                "RATING INTEGER,\n" +
-                "FOREIGN KEY (rack_id) REFERENCES BIKE_RACK(_id)," +
-                "FOREIGN KEY (user_id) REFERENCES USER(_id));");
+    BikeParkingDatabaseHelper(Context context) {
+        super(context, DB_NAME, null, DB_VERSION);
+    }
+
+    // Begin helper functions for the database
+    private static void insertRack(SQLiteDatabase db, String name,
+                                   long latitude, long longitude,
+                                   int photoID, int addedBy, int notes) {
+        ContentValues rackValues = new ContentValues();
+        rackValues.put("NAME", name);
+        rackValues.put("LATITUDE", latitude);
+        rackValues.put("LONGITUDE", longitude);
+        rackValues.put("PHOTO", photoID);
+        rackValues.put("ADDED_BY", addedBy);
+        rackValues.put("NOTES", notes);
     }
 
     /**
@@ -80,5 +49,56 @@ public class BikeParkingDatabaseHelper extends SQLiteOpenHelper {
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 
+    }
+
+    /**
+     * Called when the database is created for the first time. This is where the
+     * creation of tables and the initial population of the tables should happen.
+     *
+     * @param db The database.
+     */
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE USER ( " +
+                "_id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                "NAME TEXT, " +
+                "EMAIL TEXT, " +
+                "AUTH TEXT, " +
+                "STRIKES INTEGER);");
+        db.execSQL("CREATE TABLE BIKE_RACK (" +
+                "_id INTEGER PRIMARY KEY AUTOINCREMENT," +
+                "NAME TEXT," +
+                "LATITUDE REAL," +
+                "LONGITUDE REAL," +
+                "PHOTO INTEGER," +
+                "ADDED_BY INTEGER," +
+                "NOTES NUMERIC," +
+                "FOREIGN KEY (photo) REFERENCES PHOTO(_id)," +
+                "FOREIGN KEY (added_by) REFERENCES USER(_id));");
+        db.execSQL("CREATE TABLE CHECKIN ( " +
+                "_id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                "RACK_ID INTEGER, " +
+                "USER_ID INTEGER, " +
+                "TIME NUMERIC, " +
+                "NOTE TEXT, " +
+                "FOREIGN KEY (rack_id) REFERENCES BIKE_RACK(_id), " +
+                "FOREIGN KEY (user_id) REFERENCES USER(_id));");
+        db.execSQL("CREATE TABLE NOTES (" +
+                "_id INTEGER PRIMARY KEY AUTOINCREMENT," +
+                "RACK_ID INTEGER," +
+                "USER_ID INTEGER," +
+                "TIME NUMERIC," +
+                "SPACE_REMAINING INTEGER," +
+                "FOREIGN KEY (rack_id) REFERENCES BIKE_RACK(_id)," +
+                "FOREIGN KEY (user_id) REFERENCES USER(_id));");
+        db.execSQL("CREATE TABLE PHOTO (" +
+                "_id INTEGER PRIMARY KEY AUTOINCREMENT," +
+                "RACK_ID INTEGER,\n" +
+                "USER_ID INTEGER,\n" +
+                "TIME NUMERIC,\n" +
+                "PHOTO BLOB,\n" +
+                "RATING INTEGER,\n" +
+                "FOREIGN KEY (rack_id) REFERENCES BIKE_RACK(_id)," +
+                "FOREIGN KEY (user_id) REFERENCES USER(_id));");
     }
 }
