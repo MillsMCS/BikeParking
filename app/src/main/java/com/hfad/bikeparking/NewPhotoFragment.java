93b82a363eab276148e15c2ad5d8e50f61cf7342
kikifreena <14kobarbara@gmail.com>
2019-05-01 19:35:50 -0700

    First attempt to implement upload photo code
    Note: This needs testing.

--- app/src/main/java/com/hfad/bikeparking/NewPhotoFragment.java
+++ app/src/main/java/com/hfad/bikeparking/NewPhotoFragment.java
@@ -1,13 +1,19 @@
 package com.hfad.bikeparking;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 
 
 public class NewPhotoFragment extends Fragment {
+    private static final int CAMERA_REQUEST = 1888;
+    private View layout;
 
     public NewPhotoFragment() {
         // Required empty public constructor
@@ -16,13 +22,40 @@ public class NewPhotoFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
     }
+
+    // https://stackoverflow.com/questions/9941637/android-how-to-save-camera-images-in-database-and-display-another-activity-in-li#
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+
+        if (requestCode == CAMERA_REQUEST) {
+            Bitmap photo = (Bitmap) data.getExtras().get("data");
+            ImageView imageView = (ImageView) layout.findViewById(R.id.selected_image);
+            imageView.setImageBitmap(photo);
+
+        }
+    }
+
+}
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_new_photo, container, false);
+        View layout = inflater.inflate(R.layout.fragment_new_photo, container, false);
+
+        Button b = (Button) layout.findViewById(R.id.upload_photo);
+        b.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(cameraIntent, CAMERA_REQUEST);
+            }
+        });
+
+
+        return layout;
     }
 
 }
