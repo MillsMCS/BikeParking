a238128618261bd1f840a12ec246a3fe6c0d69e1
kikifreena <14kobarbara@gmail.com>
2019-05-09 23:56:09 -0700

    Implement upload photo button with image buttons
    Attempted to add text descriptors to them but they looked ugly so idk if we should use them.
    Also added a confirm button which would then send a POST request to the server to upload the photo. Button doesn't currently work right now

--- bikeparkingserver/get-rack.py
+++ bikeparkingserver/get-rack.py
@@ -18,9 +18,7 @@ c = conn.cursor()
 c.execute("SELECT NAME, latitude, longitude, photo, added_by, notes FROM BIKE_RACK")
 
 bike_racks = {}
-import cgitb
 
-cgitb.enable()
 for name, lati, longi, photo, added_by, notes in c.fetchall():
     if latitude == None or longitude == None:
         bike_racks[str(lati) + ", " + str(longi)] = {"name": name, 
@@ -30,7 +28,11 @@ for name, lati, longi, photo, added_by, notes in c.fetchall():
                 "added_by": added_by,
                 "notes": notes}
     elif str(lati) == str(latitude) and str(longi) == str(longitude):
+<<<<<<< HEAD
         bike_racks[str(lati) + ", " + str(longi)] = {"name": name,
+=======
+        bike_racks[name] = {"name": name,
+>>>>>>> Implement upload photo button with image buttons
                                     "latitude": lati,
                                     "longitude": longi,
                                     "photo_url": photo,
