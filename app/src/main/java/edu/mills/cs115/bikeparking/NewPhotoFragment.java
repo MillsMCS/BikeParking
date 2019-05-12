9e087abadabc5aad8bc1148cd8d44ef9538f8cb2
kikifreena <14kobarbara@gmail.com>
2019-05-11 23:18:22 -0700

    Initial commits for the server connection to upload photo
    Converts the photo to a byte array string, to be stored in the database, then later we can retrieve it and convert it back.
    
    Still to be done: get the rack ID and user ID to store it

--- app/src/main/java/edu/mills/cs115/bikeparking/NewPhotoFragment.java
+++ app/src/main/java/edu/mills/cs115/bikeparking/NewPhotoFragment.java
@@ -11,6 +11,8 @@ import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
 import android.support.v4.content.FileProvider;
+import android.util.Base64;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,10 +21,15 @@ import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
@@ -41,30 +48,27 @@ public class NewPhotoFragment extends Fragment {
     private static final int CAMERA_REQUEST = 1888;
     private static final int SELECT_IMAGE = 5871;
     private static Activity activity;
-    private static String urlString;
+    private static boolean imageUploaded = false;
+    private static Toast uploadFailureMessage;
+    private static Bitmap image;
     protected Uri photoURI;
     private View layout;
-    private static boolean imageUploaded = false;
     private Toast failureMessage;
-    private Toast uploadFailureMessage;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
     }
 
-    // https://stackoverflow.com/questions/9941637/android-how-to-save-camera-images-in-database-and-display-another-activity-in-li#
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (resultCode == RESULT_OK) {
             ImageView imageView = layout.findViewById(R.id.selected_image);
-            Bitmap bitmap = null;
             if (requestCode == CAMERA_REQUEST) {
                 try {
                     // much thanks to https://stackoverflow.com/questions/3879992/how-to-get-bitmap-from-an-uri
-                    bitmap = MediaStore.Images.Media.getBitmap(activity.getContentResolver(), photoURI);
+                    image = MediaStore.Images.Media.getBitmap(activity.getContentResolver(), photoURI);
                 } catch (IOException e) {
                     failureMessage.show();
                 }
@@ -72,19 +76,19 @@ public class NewPhotoFragment extends Fragment {
             if (requestCode == SELECT_IMAGE) {
                 try {
                     photoURI = data.getData();
-                    bitmap = MediaStore.Images.Media.getBitmap(activity.getContentResolver(), photoURI);
-                } catch (Exception e) {
+                    image = MediaStore.Images.Media.getBitmap(activity.getContentResolver(), photoURI);
+                } catch (IOException e) {
                     uploadFailureMessage.show();
                 }
             }
             imageUploaded = true;
-            imageView.setImageBitmap(bitmap);
+            imageView.setImageBitmap(image);
         } else if (requestCode == CAMERA_REQUEST && resultCode == RESULT_CANCELED) {
             failureMessage.show();
         } else if (requestCode == SELECT_IMAGE && resultCode == RESULT_CANCELED) {
             uploadFailureMessage.show();
         } else {
-            Toast.makeText(activity, "Unknown Error", Toast.LENGTH_LONG).show();
+            Toast.makeText(activity, this.getString(R.string.error_unknown), Toast.LENGTH_LONG).show();
         }
     }
 
@@ -94,7 +98,7 @@ public class NewPhotoFragment extends Fragment {
         // Inflate the layout for this fragment
         layout = inflater.inflate(R.layout.fragment_new_photo, container, false);
         activity = getActivity();
-        urlString = getString(R.string.server_url_root);
+        image = null;
         failureMessage = Toast.makeText(activity, getString(R.string.take_photo_error),
                 Toast.LENGTH_LONG);
         uploadFailureMessage = Toast.makeText(activity, getString(R.string.select_photo_error),
@@ -129,7 +133,7 @@ public class NewPhotoFragment extends Fragment {
         confirmButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 if (imageUploaded) {
-                    new uploadImageTask().execute("");
+                    new uploadImageTask().execute(image);
                 } else {
                     Toast.makeText(activity, activity.getString(R.string.image_not_uploaded_error),
                             Toast.LENGTH_SHORT).show();
@@ -176,27 +180,49 @@ public class NewPhotoFragment extends Fragment {
         }
     }
 
-    private static class uploadImageTask extends AsyncTask<String, Void, String> {
+    static class uploadImageTask extends AsyncTask<Bitmap, Void, String> {
 
         @Override
-        protected String doInBackground(String... params) {
+        protected String doInBackground(Bitmap... params) {
             // https://stackoverflow.com/questions/2938502/sending-post-data-in-android?noredirect=1&lq=1
-            String data = params[0]; //data to post
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            Bitmap bitmap = params[0];
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, baos);
+            byte[] bitmapArray = baos.toByteArray();
+            String data = Base64.encodeToString(bitmapArray, 0);
             try {
-                URL url = new URL(urlString);
+                URL url = new URL("https://naclo.cs.umass.edu/cgi-bin/bikeparkingserver/test.py");
                 HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setDoOutput(true);
+                urlConnection.setChunkedStreamingMode(0);
+                // Prepare the data to go out
                 OutputStream out = new BufferedOutputStream(urlConnection.getOutputStream());
-
-                BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(out, StandardCharsets.UTF_8));
-                writer.write(data);
-                writer.flush();
-                writer.close();
+                BufferedWriter w = new BufferedWriter(new OutputStreamWriter(out, StandardCharsets.UTF_8));
+                //Log.d("NewPhotoFragment", data);
+                w.write(data);
+                w.flush();
+                w.close();
                 out.close();
+                baos.close();
 
-                urlConnection.connect();
-                return "some information here";
+                InputStream responseStream = new BufferedInputStream(urlConnection.getInputStream());
+                BufferedReader responseStreamReader = new BufferedReader(new InputStreamReader(responseStream));
 
+                String line;
+                StringBuilder stringBuilder = new StringBuilder();
+
+                while ((line = responseStreamReader.readLine()) != null) {
+                    stringBuilder.append(line).append("\n");
+                }
+                responseStreamReader.close();
+
+                String response = stringBuilder.toString();
+                Log.d("NewPhotoFragment", "Input:" + response);
+                responseStream.close();
+                urlConnection.disconnect();
+                return response;
             } catch (Exception e) {
+                e.printStackTrace();
                 return null;
             }
         }
@@ -204,11 +230,12 @@ public class NewPhotoFragment extends Fragment {
         @Override
         protected void onPostExecute(String result) {
             if (result == null) {
-                Toast.makeText(activity, "Upload failed", Toast.LENGTH_LONG).show();
+                uploadFailureMessage.show();
+            } else {
+                // TODO: Return to calling activity
             }
         }
     }
-
 }
 
 
